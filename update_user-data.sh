9fac6ea4b7ac59c3bf257a50527930d22f854c67
tdeheurles <tdeheurles@gmail.com>
2015-08-07 11:51:42 +0000

    removed borken proxy

--- update_user-data.sh
+++ update_user-data.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-coreos-cloudinit -validate=true --from-file ./cloud-config.yml && sudo cp ~/cloud-config/cloud-config.yml /var/lib/coreos-install/user_data && sudo reboot
+./generate.sh
+coreos-cloudinit -validate=true --from-file ./cloud-config.yml && sudo cp ./cloud-config.yml /var/lib/coreos-install/user_data && sudo reboot
