30cc15da5b90311219eeeac8346a6333804f0e16
tdeheurles <tdeheurles@gmail.com>
2015-08-07 10:36:45 +0200

    Added update_user-data script

---
+++ template.config.sh
@@ -0,0 +1,12 @@
+# security
+password=''
+id_rsa=''
+
+# cluster
+discovery_token=''
+
+# server
+hostname='node01'
+
+# kubernetes
+image_kubernetes='gcr.io/google_containers/hyperkube:v1.0.1'
---
+++ update_user-data.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+coreos-cloudinit       `
+	-validate=true     `
+	--from-file        `
+	./cloud-config.yml `
+	&& sudo cp                            `
+	    ~/cloud-config/cloud-config.yml   `
+	    /var/lib/coreos-install/user_data `
+	    && sudo reboot
