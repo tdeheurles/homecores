e7b6b3d35d1544c49681619b9ab79978e4d4e8cc
tdeheurles <tdeheurles@gmail.com>
2015-08-07 16:25:51 +0200

    correction to generate.sh

--- generate.sh
+++ generate.sh
@@ -4,8 +4,8 @@
 
 cp $cloud_config_template_file $cloud_config_file
 
-sed -i "s|__PASSWORD__|$password|g"                 $out_file
-sed -i "s|__ID_RSA__|$id_rsa|g"                     $out_file
-sed -i "s|__HOSTNAME__|$hostname|g"                 $out_file
-sed -i "s|__DISCOVERY_TOKEN__|$discovery_token|g"   $out_file
-sed -i "s|__IMAGE_KUBERNETES__|$image_kubernetes|g" $out_file
+sed -i "s|__PASSWORD__|$password|g"                 $cloud_config_file
+sed -i "s|__ID_RSA__|$id_rsa|g"                     $cloud_config_file
+sed -i "s|__HOSTNAME__|$hostname|g"                 $cloud_config_file
+sed -i "s|__DISCOVERY_TOKEN__|$discovery_token|g"   $cloud_config_file
+sed -i "s|__IMAGE_KUBERNETES__|$image_kubernetes|g" $cloud_config_file
