4cd0c8a60c50a455c506e55dd6c18ae091b495be
tdeheurles <tdeheurles@gmail.com>
2015-08-10 09:37:36 +0200

    Updated coreos_config.sh sample

--- samples/sample.coreos_config.sh
+++ samples/sample.coreos_config.sh
@@ -5,12 +5,22 @@ id_rsa=''
 # cluster
 discovery_token=''
 
-# server
-hostname=''
+# server name : use something like :
+#  - vagrant: username-vm-coreos
+#  - vagrant: username-bm-coreos
+coreos_hostname="username-xm-coreos"
 
 # kubernetes
-image_kubernetes='gcr.io/google_containers/hyperkube:v1.0.2'
+image_kubernetes='gcr.io/google_containers/hyperkube:v1.0.1'
 
+# The mask of the network used
+network_mask="192.168.1"
+
+# Uncomment one of the following
+#   BareMetal :
+#user_data_file="/var/lib/coreos-install/user_data"
+#   Vagrant :
+user_data_file="/var/lib/coreos-vagrant/vagrantfile-user-data"
 
 
 # ===================== ALL IS USER CONFIGURATE AT THIS POINT =======================
@@ -21,5 +31,3 @@ main_folder="`pwd`"
 
 cloud_config_template_file="$templates/template.cloud-config.yml"
 cloud_config_file="$main_folder/cloud-config.yml"
-
-user_data_file="/var/lib/coreos-install/user_data"
