e3760dd34223ddc2f7a055bf81791928bc3ce105
tdeheurles <tdeheurles@gmail.com>
2015-08-10 15:41:38 +0200

    updated prepare_vagrant script generated files

--- prepare_vagrant.sh
+++ prepare_vagrant.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-cp samples/sample.coreos_config.sh    config.sh
-cp samples/sample.vagrant_config.rb   vagrant_config.rb
-cp samples/sample.vagrant_synced_folders.yaml synced_folders.yaml
-
+cp samples/sample.coreos_config.sh               config.sh
+cp samples/sample.vagrant_config.rb              vagrant_config.rb
+cp samples/sample.vagrant_synced_folders.yaml    vagrant_synced_folders.yaml
